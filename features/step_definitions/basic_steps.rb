158f7d6c78b4186c99311d80f1669d096bf72091
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-01-15 19:45:38 +0100

    Modify step def for cart route

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -25,6 +25,7 @@ end
 def page_path_from(page_name)
   case page_name.downcase
     when 'thaitanic' then restaurant_path(Restaurant.find_by(name: page_name))
+    when 'cart' then cart_path()  
   end
 end
 
