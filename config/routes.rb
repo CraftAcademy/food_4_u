e66dc47e41ca5e03cbb578ffb03e80830700fb84
marizaswart <mariza.swart@gmail.com>
2018-01-17 13:11:26 +0100

    Remove premature code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/charges', to: 'charges#show'
-  post '/charges', to: 'charges#create'
   root controller: :restaurants, action: :index
   resources :restaurants, only: [:show, :index]
   resources :orders, only: [:create, :update]
