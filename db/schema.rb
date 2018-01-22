662c546a546b2985b5059c362cc20dd605cf8f35
marizaswart <mariza.swart@gmail.com>
2018-01-22 13:50:42 +0100

    Update title kitchen to couch on application.rb previously food 4 u

--- db/schema.rb
+++ db/schema.rb
@@ -58,11 +58,6 @@ ActiveRecord::Schema.define(version: 20180118195504) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "street_address"
-    t.string "post_code"
-    t.string "city"
-    t.float "latitude"
-    t.float "longitude"
     t.string "assortment"
   end
 
